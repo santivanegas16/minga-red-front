4894c48af521c4a28938fe91fd37723b09c86253
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-05-24 00:16:32 -0500

    carrousel vista web

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,34 @@ function App() {
   return (
     <>
 
-      <div className="w-full h-screen lg:h-[646px] lg:-top-0.5 bg-cover bg-center relative " alt='fondo' style={{ backgroundImage: `url(${imagenFondo})` }}>
-          <nav className='flex justify-between'>
-            <img className="mt-[21px] ml-[21px] w-14 h-14 lg:mt-[34px] lg:ml-[91px]" src='/img/Menu.png' alt='menu'></img>
-            <img className="mt-6 mr-9 w-10 h-12 lg:hidden" src='/img/LogoMobile.png' alt='logo'></img>
-            <img className="mt-[44px] mr-[91px] w-[193px] h-[42px] hidden lg:block" src='/img/Logo.png' alt='logo'></img>
-          </nav>
+      <nav className="flex justify-between w-full h-screen lg:h-[646px] lg:-top-0.5 bg-cover bg-center relative " alt='fondo' style={{ backgroundImage: `url(${imagenFondo})` }}>
+        <img className="mt-[21px] ml-[21px] w-14 h-14 lg:mt-[34px] lg:ml-[91px]" src='/img/Menu.png' alt='menu'></img>
+        <img className="mt-6 mr-9 w-10 h-12 lg:hidden" src='/img/LogoMobile.png' alt='logo'></img>
+        <img className="mt-[46px] mr-[91px] w-[193px] h-[42px] hidden lg:block" src='/img/Logo.png' alt='logo'></img>
+      </nav>
 
-          <main className="text-center lg:text-start text-white flex flex-col justify-center items-center lg:items-start lg:ml-[108px] absolute inset-y-1/2 inset-x-5">
-            <h1 className='font-poppins pb-6 lg:pb-4 text-[40px] leading-10 font-bold lg:text-[64px] '>For the love of manga</h1>
-            <h5 className='font-poppins text-xl lg:text-2xl font-normal pb-6 lg:pb-4 leading-5'>Explore our varieties</h5>
-            <h5 className='font-poppins hidden lg:flex text-base font-semibold pb-6 lg:pb-4 leading-5'>#MingaLove<p className=''>❤️</p></h5>
-            <button className='bg-white text-orange-500 text-xl  cursor-pointer font-roboto font-medium py-[15.5px] w-[363px] rounded-md lg:hidden'>Let’s go!</button>
-            <button className='bg-white text-orange-500 text-2xl cursor-pointer font-roboto font-medium w-[240px] py-[13.5px] rounded-md hidden lg:block'>Sign In!</button>
-          </main>
+      <div className="text-center lg:text-start text-white flex flex-col justify-center items-center lg:items-start lg:ml-[108px] absolute inset-y-1/2 inset-x-5">
+        <h1 className='font-poppins pb-6 lg:pb-4 text-[40px] leading-10 font-bold lg:text-[64px] '>For the love of manga</h1>
+        <h5 className='font-poppins text-xl lg:text-2xl font-normal pb-6 lg:pb-4 leading-5'>Explore our varieties</h5>
+        <h5 className='font-poppins hidden lg:flex text-base font-semibold pb-6 lg:pb-4 leading-5'>#MingaLove<p className=''>❤️</p></h5>
+        <button className='bg-white text-orange-500 text-xl  cursor-pointer font-roboto font-medium py-[15.5px] w-[363px] rounded-md lg:hidden'>Let’s go!</button>
+        <button className='bg-white text-orange-500 text-2xl cursor-pointer font-roboto font-medium w-[240px] py-[13.5px] rounded-md hidden lg:block'>Sign In!</button>
+      </div>
+
+      <main className='hidden lg:block mx-[91px] mt-[57px] mb-[58px]'>
+        <div className='h-[265px] bg-orange-500 rounded-md flex items-center relative'>
+          <img className="absolute left-8" src='/img/prev.png' alt='menu'></img>
+          <img className="absolute left-9" src='/img/carrusel1.png' alt='menu'></img>
+          <img className="absolute left-[37%] bottom-[22px] xl:left-[379px] 2xl:right-[57%] 2xl:left-auto" src='/img/carrusel2.png' alt='menu'></img>
+          <div className='flex flex-col w-[26%] text-white absolute left-[63%] xl:w-[27%] xl:left-[704px] 2xl:w-[356px] 2xl:right-[18%] 2xl:left-auto'>
+            <h4 className='font-roboto text-2xl font-medium mb-2.5'>Shonen</h4>
+            <p className='font-roboto text-sm font-normal leading-[13.33px]'>Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
+          </div>
+          <img className="absolute right-8" src='/img/next.png' alt='menu'></img>
+
         </div>
+
+      </main>
 
       <footer>
         <div className="mt-5 mb-3 flex justify-center items-center">
@@ -38,8 +51,7 @@ function App() {
             <a href='#'><img className="px-5" src='/img/Youtube.svg' alt='logo' /></a>
           </div>
           <button className='flex justify-center items-center bg-orange-500 text-white text-base cursor-pointer font-poppins font-normal leading-4 h-[43px] w-[221px] mb-5 rounded-md'>Donate <img className="ml-5 mt-1" src='/img/Union.svg' alt='union' /></button>
-          <hr className="w-4/5 mx-auto lg:block text-[#222222] border-[#2B3D51] opacity-30"></hr>
-
+          <hr className="w-4/5 mx-auto hidden lg:block text-[#222222] border-[#2B3D51] opacity-30"></hr>
         </div>
       </footer>
     </>
