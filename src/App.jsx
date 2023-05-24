e7cfcefd94e554cb86d7fb23eed11c97f19b6c56
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-05-24 01:11:37 -0500

    vista web terminada

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,15 @@ function App() {
         <img className="mt-[21px] ml-[21px] w-14 h-14 lg:mt-[34px] lg:ml-[91px]" src='/img/Menu.png' alt='menu'></img>
         <img className="mt-6 mr-9 w-10 h-12 lg:hidden" src='/img/LogoMobile.png' alt='logo'></img>
         <img className="mt-[46px] mr-[91px] w-[193px] h-[42px] hidden lg:block" src='/img/Logo.png' alt='logo'></img>
-      </nav>
 
-      <div className="text-center lg:text-start text-white flex flex-col justify-center items-center lg:items-start lg:ml-[108px] absolute inset-y-1/2 inset-x-5">
-        <h1 className='font-poppins pb-6 lg:pb-4 text-[40px] leading-10 font-bold lg:text-[64px] '>For the love of manga</h1>
-        <h5 className='font-poppins text-xl lg:text-2xl font-normal pb-6 lg:pb-4 leading-5'>Explore our varieties</h5>
-        <h5 className='font-poppins hidden lg:flex text-base font-semibold pb-6 lg:pb-4 leading-5'>#MingaLove<p className=''>❤️</p></h5>
-        <button className='bg-white text-orange-500 text-xl  cursor-pointer font-roboto font-medium py-[15.5px] w-[363px] rounded-md lg:hidden'>Let’s go!</button>
-        <button className='bg-white text-orange-500 text-2xl cursor-pointer font-roboto font-medium w-[240px] py-[13.5px] rounded-md hidden lg:block'>Sign In!</button>
-      </div>
+        <div className="text-center lg:text-start text-white flex flex-col justify-center items-center lg:items-start lg:ml-[108px] absolute inset-y-1/2 inset-x-5">
+          <h1 className='font-poppins pb-6 lg:pb-4 text-[40px] leading-10 font-bold lg:text-[64px] '>For the love of manga</h1>
+          <h5 className='font-poppins text-xl lg:text-2xl font-normal pb-6 lg:pb-4 leading-5'>Explore our varieties</h5>
+          <h5 className='font-poppins hidden lg:flex text-base font-semibold pb-6 lg:pb-4 leading-5'>#MingaLove<p className=''>❤️</p></h5>
+          <button className='bg-white text-orange-500 text-xl  cursor-pointer font-roboto font-medium py-[15.5px] w-[363px] rounded-md lg:hidden'>Let’s go!</button>
+          <button className='bg-white text-orange-500 text-2xl cursor-pointer font-roboto font-medium w-[240px] py-[13.5px] rounded-md hidden lg:block'>Sign In!</button>
+        </div>
+      </nav>
 
       <main className='hidden lg:block mx-[91px] mt-[57px] mb-[58px]'>
         <div className='h-[265px] bg-orange-500 rounded-md flex items-center relative'>
@@ -31,28 +31,29 @@ function App() {
             <p className='font-roboto text-sm font-normal leading-[13.33px]'>Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
           </div>
           <img className="absolute right-8" src='/img/next.png' alt='menu'></img>
-
         </div>
-
       </main>
 
       <footer>
-        <div className="mt-5 mb-3 flex justify-center items-center">
-          <a className="px-16 text-base font-poppins font-normal leading-6 text-[#222222]" href='#'>Home</a>
-          <a className="px-16 text-base font-poppins font-normal leading-6 text-[#222222]" href='#'>Mangas</a>
-        </div>
-        <hr className="w-4/5 mx-auto lg:hidden text-[#222222] border-[#2B3D51] opacity-30"></hr>
-        <div className="my-4 flex flex-col justify-center items-center">
-          <img className="" src='/img/LogoDosP.png' alt='logo' />
-          <div className="my-5 flex justify-center items-center">
-            <a href='#'><img className="px-5" src='/img/Facebook.svg' alt='logo' /></a>
-            <a href='#'><img className="px-5" src='/img/Twitter.svg' alt='logo' /></a>
-            <a href='#'><img className="px-5" src='/img/Vimeo.svg' alt='logo' /></a>
-            <a href='#'><img className="px-5" src='/img/Youtube.svg' alt='logo' /></a>
+        <img className="w-full h-full hidden lg:block" src='/img/Footer.svg' alt='footer'></img>
+        <div className='flex flex-col w-4/5 mx-auto mt-10 lg:mt-10 justify-center items-center lg:flex-row lg:justify-between'>
+          <div className="mt-5 mb-3 flex justify-center items-center">
+            <a className="px-[62px] text-base font-poppins font-normal leading-6 text-[#222222] lg:px-0" href='#'>Home</a>
+            <a className="px-[62px] text-base font-poppins font-normal leading-6 text-[#222222] lg:pl-[62px] lg:pr-0" href='#'>Mangas</a>
           </div>
-          <button className='flex justify-center items-center bg-orange-500 text-white text-base cursor-pointer font-poppins font-normal leading-4 h-[43px] w-[221px] mb-5 rounded-md'>Donate <img className="ml-5 mt-1" src='/img/Union.svg' alt='union' /></button>
-          <hr className="w-4/5 mx-auto hidden lg:block text-[#222222] border-[#2B3D51] opacity-30"></hr>
+          <hr className="w-4/5 mx-auto lg:hidden text-[#222222] border-[#2B3D51] opacity-30"></hr>
+          <img className="mt-5" src='/img/LogoDosP.png' alt='logo' />
+          <div className=" flex flex-col justify-center items-center">
+            <div className="flex flex-row my-6">
+              <a href='#'><img className="px-5 lg:pl-0 lg:pr-5" src='/img/Facebook.svg' alt='logo' /></a>
+              <a href='#'><img className="px-5" src='/img/Twitter.svg' alt='logo' /></a>
+              <a href='#'><img className="px-5" src='/img/Vimeo.svg' alt='logo' /></a>
+              <a href='#'><img className="px-5 lg:pl-5 lg:pr-0" src='/img/Youtube.svg' alt='logo' /></a>
+            </div>
+            <button className='mb-10 lg:mb-5 flex justify-center items-center bg-orange-500 text-white text-base cursor-pointer font-poppins font-normal leading-4 h-[43px] w-[221px] rounded-md'>Donate <img className="ml-5 mt-1" src='/img/Union.svg' alt='union' /></button>
+          </div>
         </div>
+        <hr className="mb-24 w-4/5 mx-auto hidden lg:block text-[#222222] border-[#2B3D51] opacity-30"></hr>
       </footer>
     </>
   )
