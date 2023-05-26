b87d40992c75828ecdf1d39105202580b34c5fb2
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-05-25 19:11:02 -0500

    se corrige logo web

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <nav className="flex justify-between w-full h-screen lg:h-[646px] lg:-top-0.5 bg-cover bg-center relative " alt='fondo' style={{ backgroundImage: `url(${imagenFondo})` }}>
         <img className="mt-[21px] ml-[21px] w-14 h-14 lg:mt-[34px] lg:ml-[91px]" src='/img/Menu.png' alt='menu'></img>
         <img className="mt-6 mr-9 w-10 h-12 lg:hidden" src='/img/LogoMobile.png' alt='logo'></img>
-        <img className="mt-[46px] mr-[91px] w-[193px] h-[42px] hidden lg:block" src='/img/Logo.png' alt='logo'></img>
+        <img className="mt-[46px] mr-[91px] w-[193px] h-[42px] hidden lg:block" src='/img/logo.png' alt='logo'></img>
 
         <div className="text-center lg:text-start text-white flex flex-col justify-center items-center lg:items-start lg:ml-[108px] absolute inset-y-1/2 inset-x-5">
           <h1 className='font-poppins pb-6 lg:pb-4 text-[40px] leading-10 font-bold lg:text-[64px] '>For the love of manga</h1>
