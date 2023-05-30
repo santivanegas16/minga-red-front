dbb5b32174c95d5178f083f794db4cf51a9222fb
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-05-29 19:54:44 -0500

    se corrige navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-
+import imagenFondo from '/img/Background1.png';
 import Navbar from './components/Navbar';
-import Welcome from './components/Welcome';
-
-import Carousel from './components/Carousel';
 import Footer from './components/Footer';
 import Index from './pages/Index';
 
@@ -12,17 +9,18 @@ function App() {
   const [count, setCount] = useState(0)
 
   let data = [{
-      name:"Shonen",
-      character_photo:"/img/carrusel1.png",
-      cover_photo:"/img/carrusel2.png",
-      description: "Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out."
+    name: "Shonen",
+    character_photo: "/img/carrusel1.png",
+    cover_photo: "/img/carrusel2.png",
+    description: "Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out."
   }]
 
   return (
     <>
-
+      <div className="w-full h-screen lg:h-[646px] lg:-top-0.5 bg-cover bg-center" alt='fondo' style={{ backgroundImage: `url(${imagenFondo})` }}>
+      </div>
       <Navbar />
-      <Index data = {data} />
+      <Index data={data} />    
       <Footer />
     </>
   )
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,6 @@
 export default function Welcome() {
   return (
-    <div className="text-center lg:text-start text-white flex flex-col justify-center items-center lg:items-start lg:ml-[108px] absolute inset-y-1/2 inset-x-5">
+    <div className="text-center lg:text-start text-white flex flex-col justify-center items-center lg:items-start lg:ml-[108px] absolute inset-y-1/2 inset-x-5 lg:block  lg:top-[225px]">
         <h1 className='font-poppins pb-6 lg:pb-4 text-[40px] leading-10 font-bold lg:text-[64px] '>For the love of manga</h1>
         <h5 className='font-poppins text-xl lg:text-2xl font-normal pb-6 lg:pb-4 leading-5'>Explore our varieties</h5>
         <h5 className='font-poppins hidden lg:flex text-base font-semibold pb-6 lg:pb-4 leading-5'>#MingaLove<p className=''>❤️</p></h5>
