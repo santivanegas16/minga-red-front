e466b251ae7354029780f5102bcecc7edf37606d
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-06-05 22:23:53 -0500

    ok display

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
+import Display from './Display';
+import Menu from '/img/Menu.png';
+import LogoMobile from '/img/LogoMobile.png';
+import logo from '/img/logo.png';
+import { useState } from 'react';
+
 export default function Navbar() {
+    let options = [
+        { title: "Home" },
+        { title: "Comics" },
+        { title: "My Comics" },
+        { title: "Favorites" },
+        { title: "Logout" }
+    ]
+    const [show, setShow] = useState(false)
     return (
-        <nav className="flex justify-between w-full absolute top-0">
-            <img className="mt-[21px] ml-[21px] w-14 h-14 lg:mt-[34px] lg:ml-[91px]" src='/img/Menu.png' alt='menu'></img>
-            <img className="mt-6 mr-9 w-10 h-12 lg:hidden" src='/img/LogoMobile.png' alt='logo'></img>
-            <img className="mt-[46px] mr-[91px] w-[193px] h-[42px] hidden lg:block" src='/img/logo.png' alt='logo'></img>
-        </nav>
+        <>
+            {show && <Display options={options} show={show} setShow={setShow}/>}
+            <nav className="flex justify-between w-full absolute top-0">
+                <img onClick={() => setShow(!show)} className=" cursor-pointer mt-[21px] ml-[21px] w-14 h-14 lg:mt-[34px] lg:ml-[91px]" src={Menu} alt='menu'></img>
+                <img className="mt-6 mr-9 w-10 h-12 lg:hidden" src={LogoMobile} alt='logo'></img>
+                <img className="mt-[46px] mr-[91px] w-[193px] h-[42px] hidden lg:block" src={logo} alt='logo'></img>
+            </nav>
+        </>
     )
 }
