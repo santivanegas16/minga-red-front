71f72eacb7998b9d738630256982b316af097ce8
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-06-07 23:28:55 -0500

    se corrige codigo extra

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,11 @@ import Index from './pages/Index';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  let data = [{
-    name: "Shonen",
-    character_photo: "/img/carrusel1.png",
-    cover_photo: "/img/carrusel2.png",
-    description: "Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out."
-  }]
 
   return (
     <>
       <Main>
-        <Index data={data} />
+        <Index />
       </Main>
     </>
   )
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,13 +2,13 @@ import Welcome from "../components/Welcome"
 import Carousel from "../components/Carousel"
 import imagenFondo from '/img/Background1.png';
 
-export default function Index({ data }) {
+export default function Index() {
     return (
         <>
             <div className="w-full h-screen lg:h-[646px] lg:-top-0.5 bg-cover bg-center" alt='fondo' style={{ backgroundImage: `url(${imagenFondo})` }}></div>
             <Welcome />
             <main className='hidden lg:block mx-[91px] mt-[57px] mb-[58px]'>
-                <Carousel name={data[0].name} character_photo={data[0].character_photo} cover_photo={data[0].cover_photo} description={data[0].description} />
+                <Carousel />
             </main>
         </>
     )
