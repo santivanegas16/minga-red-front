fa080c20999220d885faf6517ff04787a70d2179
JALZ <jannus.largo@correounivalle.edu.co>
2023-06-27 14:31:18 -0500

    Register and Login were designed, the form data is also captured

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -1,25 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import Index from './Index'
-import SignIn from './SignIn'
+import Register from './Register'
 import CiaForm from './CiaForm'
 import Main from '../layouts/Main'
 import AuthorForm from './AuthorForm'
 import MangaForm from './MangaForm';
+import Login from './Login';
 
 const router = createBrowserRouter([
     //necesita que pasemos un array de objetos
     //cada objeto tendra la propiedad PATH con la ruta y ELEMENT con el elemento que renderiza ese path
-    {path: '/', // con una ruta
-     element: <Main />, // renderizo un componente de tipo layaout
-     children: [ // cuyos hijes van a ser todas las interfaces que tenga la app
-        { path: '/', element: <Index/>},
-        { path: '/index', element: <Index/>},
-        { path: '/home', element: <Index/>},
-        { path: '/signin', element: <SignIn/>},
-        { path: '/cia-form', element: <CiaForm/> },
-        { path: '/author-form', element: <AuthorForm/>},
-        { path: '/manga-form', element: <MangaForm/>},
-    ]}
+    {
+        path: '/', // con una ruta
+        element: <Main />, // renderizo un componente de tipo layaout
+        children: [ // cuyos hijes van a ser todas las interfaces que tenga la app
+            { path: '/', element: <Index /> },
+            { path: '/index', element: <Index /> },
+            { path: '/home', element: <Index /> },
+            { path: '/register', element: <Register /> },
+            { path: '/login', element: <Login /> },
+            { path: '/cia-form', element: <CiaForm /> },
+            { path: '/author-form', element: <AuthorForm /> },
+            { path: '/manga-form', element: <MangaForm /> },
+        ]
+    }
 ])
 
 export default router
