24fd4b47d5cfa637677b7b54abe2e8c98f5db015
Jannus Alejandro Largo Zafra <96785247+JALZ2003@users.noreply.github.com>
2023-06-28 20:49:15 -0500

    M01 view (#20)
    * The login and signout were done, but there is still a need to improve
    
    * the navigate was put back on

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,7 +6,7 @@ export default function Welcome() {
       <h1 className='font-poppins pb-6 lg:pb-4 text-[40px] leading-10 font-bold lg:text-[64px] '>For the love of manga</h1>
       <h5 className='font-poppins text-xl lg:text-2xl font-normal pb-6 lg:pb-4 leading-5'>Explore our varieties</h5>
       <h5 className='font-poppins hidden lg:flex text-base font-semibold pb-6 lg:pb-4 leading-5'>#MingaLove<p className=''>❤️</p></h5>
-      <Anchor to='/register' className='bg-white text-orange-500 text-xl lg:text-2xl cursor-pointer font-roboto font-medium py-[15.5px] lg:py-[11.5px] w-[363px] max-[380px]:w-[300px] lg:w-[240px] rounded-md lg:block lg:text-center'>Sign In!</Anchor>
+      <Anchor to='/login' className='bg-white text-orange-500 text-xl lg:text-2xl cursor-pointer font-roboto font-medium py-[15.5px] lg:py-[11.5px] w-[363px] max-[380px]:w-[300px] lg:w-[240px] rounded-md lg:block lg:text-center'>Sign In!</Anchor>
     </div>
   )
 }
