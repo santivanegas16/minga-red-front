05a6c2d66da04b5f8d673e166030a676dea4196f
JALZ <jannus.largo@correounivalle.edu.co>
2023-08-01 23:48:11 -0500

    Fix in mangas

--- src/components/Category_mangas.jsx
+++ src/components/Category_mangas.jsx
@@ -1,15 +1,14 @@
 export default function Category({ name, color, hover, value, action, isChecked }) {
-    console.log(isChecked);
     return (
         isChecked ? (<label className='relative w-[75px] h-[35px] rounded-[50px] font-poppins font-medium text-[12px] leading-[11.42px] text-center'
-            style={{ backgroundColor: `${color}`, color: `${hover}` }}> 
-            <span className="absolute inset-x-[20%] top-3">{name}</span> 
-            <input className="w-full h-full hidden" onChange={action} value={name} id={value} type="checkbox" defaultChecked={isChecked} />
+            style={{ backgroundColor: `${color}`, color: `${hover}` }}>
+            <span className="absolute inset-x-[20%] top-3">{name}</span>
+            <input className="w-full h-full" onChange={action} value={name} id={value} type="checkbox" checked={isChecked} />
         </label>) : (
             <label className='cursor-pointer relative w-[75px] h-[35px] rounded-[50px] font-poppins font-medium text-[12px] leading-[11.42px] text-center'
                 style={{ backgroundColor: `${hover}`, color: `${color}` }}>
-                    <span className="absolute inset-x-[20%] top-3">{name}</span> 
-                <input className="w-full h-full hidden" onChange={action} value={name} id={value} type="checkbox" defaultChecked={isChecked} />
+                <span className="absolute inset-x-[20%] top-3">{name}</span>
+                <input className="w-full h-full" onChange={action} value={name} id={value} type="checkbox" checked={isChecked} />
             </label>)
     )
 }
