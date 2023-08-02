83e082b5cee89a657f4441c26197f9ed4f6e1997
Jannus Alejandro Largo Zafra <96785247+JALZ2003@users.noreply.github.com>
2023-08-01 23:49:19 -0500

    Fix in mangas (#54)

--- src/pages/Mangas.jsx
+++ src/pages/Mangas.jsx
@@ -39,6 +39,7 @@ export default function Mangas() {
             setMangas(res.data.response.mangas);
             setNext(res.data.response.next);
             setPrev(res.data.response.prev);
+            actionNextOrPrev(1);
         }).catch(error => {
             console.log(error)
             setMangas([])
@@ -60,12 +61,10 @@ export default function Mangas() {
         });
     }, [page])
 
-    const actionsChecks = (e) => {
+    const actionsChecks = () => {
         let checks = Object.values(inputsChecked.current).filter(each => each.checked).map(each => each.id);
         dispatch(save_checks({ checks }));
     }
-
-    console.log(store.mangas.checks);
 
     return (
         <main className='flex flex-col items-center min-h-screen bg-[#EBEBEB] '>
@@ -81,20 +80,18 @@ export default function Mangas() {
             </div>
             <div className='bg-[#EBEBEB] lg:bg-white lg:w-[95%] lg:rounded-t-[30px] lg:rounded-b-[30px] lg:-top-[50px] relative -top-[70px] w-full rounded-t-[80px] flex flex-col items-center'>
                 <form ref={inputsChecked} className='flex h-[40px] w-[90%] sm:w-[50%] md:w-[40%] xl:w-[30%] justify-between mt-12'>
-                    <input className="bg-[#cacaca] cursor-pointer w-[75px] h-[35px] rounded-[50px] text-white font-poppins font-medium text-[12px] leading-[11.42px] text-center hidden lg:block" type='button' value="All" />
-                    {categories.map(category => <Category key={category._id} name={category.name} color={category.color} hover={category.hover} value={category._id} action={(e) => { actionsChecks(e) }} isChecked={store.mangas.checks.includes(category._id)} />)}
+                    <input key={0} className="bg-[#cacaca] cursor-pointer w-[75px] h-[35px] rounded-[50px] text-white font-poppins font-medium text-[12px] leading-[11.42px] text-center hidden lg:block" type='button' value="All" onClick={() => dispatch(save_checks({ checks: [] }))} />
+                    {categories.map(category => <Category key={category._id} name={category.name} color={category.color} hover={category.hover} value={category._id} action={() => { actionsChecks() }} isChecked={store.mangas.checks.includes(category._id)} />)}
                 </form>
-
                 {(mangas.length !== 0) ?
                     <div className='grid grid-cols-1 lg:grid-cols-2 w-full md:w-[70%] mt-2 justify-items-center'>
                         {(mangas.map((manga) =>
-                        <Card manga_id={manga._id} key={manga._id} title={manga.title} image={manga.cover_photo} type={manga.category_id.name} color={manga.category_id.color} hover={manga.category_id.hover} />
+                            <Card manga_id={manga._id} key={manga._id} title={manga.title} image={manga.cover_photo} type={manga.category_id.name} color={manga.category_id.color} hover={manga.category_id.hover} />
                         )
                         )}
                     </div> : (
                         <div className='w-full h-[244px] text-xl text-bold font-[40px] flex items-center justify-center text-center'>Manga not found</div>
                     )}
-
                 <div className="flex justify-center items-center pt-5">
                     {prev && <button className='bg-gradient-to-r from-[#FF5722] to-[#F97316] text-white rounded-full m-5 w-[100px] h-[40px] transition hover:scale-110' value={prev} onClick={(e) => { actionNextOrPrev(e.target.value) }}>Prev</button>}
                     {next && <button className='bg-gradient-to-r from-[#FF5722] to-[#F97316] text-white rounded-full m-5 w-[100px] h-[40px] transition hover:scale-110' value={next} onClick={(e) => { actionNextOrPrev(e.target.value) }}>Next</button>}
