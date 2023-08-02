609dbc7b7ee0fb6ea098f5c4e6ee3ae9796ac1fd
Jannus Alejandro Largo Zafra <96785247+JALZ2003@users.noreply.github.com>
2023-08-01 19:43:59 -0500

    Fix solve in Mangas.jsx and added Redux of checks (#52)

--- src/pages/Mangas.jsx
+++ src/pages/Mangas.jsx
@@ -1,6 +1,6 @@
 import Bg_mangas from '../../public/img/Bg_mangas.png';
 import Search from '../../public/img/Search_mangas.svg';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import apiUrl from '../apiUrl.js';
@@ -10,9 +10,11 @@ import Category from '../components/Category_mangas';
 import { useSelector, useDispatch } from 'react-redux';
 import mangasActions from '../store/actions/mangas';
 const { save_title, save_checks } = mangasActions;
-let checkeds = [];
 
 export default function Mangas() {
+
+
+    const inputsChecked = useRef();
 
     const store = useSelector(store => store)
     const dispatch = useDispatch();
@@ -33,23 +35,27 @@ export default function Mangas() {
     }, [])
 
     useEffect(() => {
-        axios.get(apiUrl + `mangas?title=${store.mangas.text}&page=${page}&category=${checkeds.join(',')}`, Headers()).then(res => {
+        axios.get(apiUrl + `mangas?title=${store.mangas.text}&page=1&category=${store.mangas.checks.join(',')}`, Headers()).then(res => {
             setMangas(res.data.response.mangas);
             setNext(res.data.response.next);
             setPrev(res.data.response.prev);
         }).catch(error => console.log(error));
-    }, [store.mangas.text, page, checkeds])
+    }, [store.mangas.text, store.mangas.checks])
+
+    useEffect(() => {
+        axios.get(apiUrl + `mangas?title=${store.mangas.text}&page=${page}&category=${store.mangas.checks.join(',')}`, Headers()).then(res => {
+            setMangas(res.data.response.mangas);
+            setNext(res.data.response.next);
+            setPrev(res.data.response.prev);
+        }).catch(error => console.log(error));
+    }, [page])
 
     const actionsChecks = (e) => {
-        if (!checkeds.includes(e.target.id)) {
-            checkeds.push(e.target.id)
-        } else {
-            checkeds = checkeds.filter(element => element !== e.target.id)
-        }
-        console.log(checkeds.join(','));
-        // dispatch(save_checks({ checks: checkeds }));
-        // console.log(store.mangas.checks)
+        let checks = Object.values(inputsChecked.current).filter(each => each.checked).map(each => each.id);
+        dispatch(save_checks({ checks }));
     }
+
+    console.log(store.mangas.checks);
 
     return (
         <main className='flex flex-col items-center min-h-screen bg-[#EBEBEB] '>
@@ -64,9 +70,9 @@ export default function Mangas() {
                 </div>
             </div>
             <div className='bg-[#EBEBEB] lg:bg-white lg:w-[95%] lg:rounded-t-[30px] lg:rounded-b-[30px] lg:-top-[50px] relative -top-[70px] w-full rounded-t-[80px] flex flex-col items-center'>
-                <div className='flex h-[40px] w-[90%] sm:w-[50%] md:w-[40%] xl:w-[30%] justify-between mt-12'>
-                    {categories.map(category => <Category key={category._id} name={category.name} color={category.color} hover={category.hover} value={category._id} action={(e) => { actionsChecks(e) }} />)}
-                </div>
+                <form ref={inputsChecked} className='flex h-[40px] w-[90%] sm:w-[50%] md:w-[40%] xl:w-[30%] justify-between mt-12'>
+                    {categories.map(category => <Category key={category._id} name={category.name} color={category.color} hover={category.hover} value={category._id} action={(e) => { actionsChecks(e) }} isChecked={store.mangas.checks.includes(category._id)} />)}
+                </form>
                 <div className='grid grid-cols-1 lg:grid-cols-2 w-full md:w-[70%] mt-2 justify-items-center'>
                     {mangas.map((manga) => <Card manga_id={manga._id} key={manga._id} title={manga.title} image={manga.cover_photo} type={manga.category_id.name} color={manga.category_id.color} />)}
                 </div>
