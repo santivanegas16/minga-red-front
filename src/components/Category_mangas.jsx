30cbc351e4924705d5ba8ea3184ba91f90bbc921
JALZ <jannus.largo@correounivalle.edu.co>
2023-08-01 19:42:49 -0500

    Fix solve in Mangas.jsx and added Redux of checks

--- src/components/Category_mangas.jsx
+++ src/components/Category_mangas.jsx
@@ -1,6 +1,13 @@
-export default function Category({ name, color, hover, value, action }) {
+export default function Category({ name, color, hover, value, action, isChecked }) {
+    console.log(isChecked);
     return (
-        <input style={{ backgroundColor: `${hover}`, color: `${color}` }} onClick={action} value={name} id={value} type="button"
-            className='w-[75px] h-[35px] rounded-[50px] font-poppins font-medium text-[12px] leading-[11.42px] text-center' />
+        isChecked ? (<label className='w-[75px] h-[35px] rounded-[50px] font-poppins font-medium text-[12px] leading-[11.42px] text-center'
+            style={{ backgroundColor: `${color}`, color: `${hover}` }}> {name}
+            <input onChange={action} value={name} id={value} type="checkbox" defaultChecked={isChecked} />
+        </label>) : (
+            <label className='w-[75px] h-[35px] rounded-[50px] font-poppins font-medium text-[12px] leading-[11.42px] text-center'
+                style={{ backgroundColor: `${hover}`, color: `${color}` }}> {name}
+                <input onChange={action} value={name} id={value} type="checkbox" defaultChecked={isChecked} />
+            </label>)
     )
 }
