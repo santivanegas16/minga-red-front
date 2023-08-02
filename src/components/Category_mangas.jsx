3a715cfb10b6f343b5d52ce325334ae91b32acc3
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-08-01 22:54:26 -0500

    fix styles in general

--- src/components/Category_mangas.jsx
+++ src/components/Category_mangas.jsx
@@ -1,13 +1,15 @@
 export default function Category({ name, color, hover, value, action, isChecked }) {
     console.log(isChecked);
     return (
-        isChecked ? (<label className='w-[75px] h-[35px] rounded-[50px] font-poppins font-medium text-[12px] leading-[11.42px] text-center'
-            style={{ backgroundColor: `${color}`, color: `${hover}` }}> {name}
-            <input onChange={action} value={name} id={value} type="checkbox" defaultChecked={isChecked} />
+        isChecked ? (<label className='relative w-[75px] h-[35px] rounded-[50px] font-poppins font-medium text-[12px] leading-[11.42px] text-center'
+            style={{ backgroundColor: `${color}`, color: `${hover}` }}> 
+            <span className="absolute inset-x-[20%] top-3">{name}</span> 
+            <input className="w-full h-full hidden" onChange={action} value={name} id={value} type="checkbox" defaultChecked={isChecked} />
         </label>) : (
-            <label className='w-[75px] h-[35px] rounded-[50px] font-poppins font-medium text-[12px] leading-[11.42px] text-center'
-                style={{ backgroundColor: `${hover}`, color: `${color}` }}> {name}
-                <input onChange={action} value={name} id={value} type="checkbox" defaultChecked={isChecked} />
+            <label className='cursor-pointer relative w-[75px] h-[35px] rounded-[50px] font-poppins font-medium text-[12px] leading-[11.42px] text-center'
+                style={{ backgroundColor: `${hover}`, color: `${color}` }}>
+                    <span className="absolute inset-x-[20%] top-3">{name}</span> 
+                <input className="w-full h-full hidden" onChange={action} value={name} id={value} type="checkbox" defaultChecked={isChecked} />
             </label>)
     )
 }
--- src/pages/Mangas.jsx
+++ src/pages/Mangas.jsx
@@ -39,7 +39,12 @@ export default function Mangas() {
             setMangas(res.data.response.mangas);
             setNext(res.data.response.next);
             setPrev(res.data.response.prev);
-        }).catch(error => console.log(error));
+        }).catch(error => {
+            console.log(error)
+            setMangas([])
+            setNext(null);
+            setPrev(null);
+        });
     }, [store.mangas.text, store.mangas.checks])
 
     useEffect(() => {
@@ -47,7 +52,12 @@ export default function Mangas() {
             setMangas(res.data.response.mangas);
             setNext(res.data.response.next);
             setPrev(res.data.response.prev);
-        }).catch(error => console.log(error));
+        }).catch(error => {
+            console.log(error)
+            setMangas([])
+            setNext(null);
+            setPrev(null);
+        });
     }, [page])
 
     const actionsChecks = (e) => {
@@ -71,11 +81,20 @@ export default function Mangas() {
             </div>
             <div className='bg-[#EBEBEB] lg:bg-white lg:w-[95%] lg:rounded-t-[30px] lg:rounded-b-[30px] lg:-top-[50px] relative -top-[70px] w-full rounded-t-[80px] flex flex-col items-center'>
                 <form ref={inputsChecked} className='flex h-[40px] w-[90%] sm:w-[50%] md:w-[40%] xl:w-[30%] justify-between mt-12'>
+                    <input className="bg-[#cacaca] cursor-pointer w-[75px] h-[35px] rounded-[50px] text-white font-poppins font-medium text-[12px] leading-[11.42px] text-center hidden lg:block" type='button' value="All" />
                     {categories.map(category => <Category key={category._id} name={category.name} color={category.color} hover={category.hover} value={category._id} action={(e) => { actionsChecks(e) }} isChecked={store.mangas.checks.includes(category._id)} />)}
                 </form>
-                <div className='grid grid-cols-1 lg:grid-cols-2 w-full md:w-[70%] mt-2 justify-items-center'>
-                    {mangas.map((manga) => <Card manga_id={manga._id} key={manga._id} title={manga.title} image={manga.cover_photo} type={manga.category_id.name} color={manga.category_id.color} />)}
-                </div>
+
+                {(mangas.length !== 0) ?
+                    <div className='grid grid-cols-1 lg:grid-cols-2 w-full md:w-[70%] mt-2 justify-items-center'>
+                        {(mangas.map((manga) =>
+                        <Card manga_id={manga._id} key={manga._id} title={manga.title} image={manga.cover_photo} type={manga.category_id.name} color={manga.category_id.color} hover={manga.category_id.hover} />
+                        )
+                        )}
+                    </div> : (
+                        <div className='w-full h-[244px] text-xl text-bold font-[40px] flex items-center justify-center text-center'>Manga not found</div>
+                    )}
+
                 <div className="flex justify-center items-center pt-5">
                     {prev && <button className='bg-gradient-to-r from-[#FF5722] to-[#F97316] text-white rounded-full m-5 w-[100px] h-[40px] transition hover:scale-110' value={prev} onClick={(e) => { actionNextOrPrev(e.target.value) }}>Prev</button>}
                     {next && <button className='bg-gradient-to-r from-[#FF5722] to-[#F97316] text-white rounded-full m-5 w-[100px] h-[40px] transition hover:scale-110' value={next} onClick={(e) => { actionNextOrPrev(e.target.value) }}>Next</button>}
