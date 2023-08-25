c266f0932f8ce1dd4c23b9c449bc3a3967768824
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-08-24 22:26:36 -0500

    fix modal comments mobile

--- src/components/Modal_comments.jsx
+++ src/components/Modal_comments.jsx
@@ -4,9 +4,9 @@ import ListComments from "./ListComments"
 export default function Modal_comments({show, setShow, reload, setReload}) {
     return (
         <>
-            <div className="fixed top-0 md:bg-black bg-[#EBEBEB] w-full h-full md:opacity-50"></div>
-            <div className='fixed z-10 inset-y-[10%] md:inset-x-[25%]  w-full h-full'>
-                <div className="relative rounded-2xl w-full md:w-[50%] h-[80%] bg-[#EBEBEB] overflow-auto ">
+            <div className="fixed z-10 top-0 md:bg-black bg-[#EBEBEB] w-full h-full md:opacity-50"></div>
+            <div className='md:fixed absolute pt-[80px] md:pt-0 z-10 inset-y-[10%] md:inset-x-[25%]  w-full h-full'>
+                <div className="relative rounded-2xl w-full md:w-[50%] md:h-[80%] h-full bg-[#EBEBEB] md:overflow-auto  ">
                     <div onClick={() => setShow(!show)} className="cursor-pointer absolute bg-orange-500 hover:bg-orange-700 p-1.5 rounded-full right-[15px] top-[15px]"><img src={Close} alt='close' /></div>
                     <ListComments reload={reload} setReload={setReload}/>
                 </div>
