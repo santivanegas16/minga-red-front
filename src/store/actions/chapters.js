4f86e97cd42ee7397e66adc6401b07b468358087
Guillermo Duque <guille980627@gmail.com>
2023-08-27 22:55:56 -0500

    get_me

--- src/store/actions/chapters.js
+++ src/store/actions/chapters.js
@@ -1,18 +1,26 @@
 import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import apiUrl from "../../apiUrl";
+import headers from '../../header.js'
 
 let saveChapters = createAsyncThunk( 'saveChapters',async (obj)=> {
-    // AGREGAR ESTRUCTURA TRY CATCH
-    let headers = { headers: { 'Authorization': `Bearer ${localStorage.getItem("token")}` } }
-    let res = await axios(apiUrl + "chapters/me?manga_id=" + obj.manga_id, headers)
-    console.log(res.data.response)
-    return { 
-        title:res.data.response[0].title,
-        cover_photo:res.data.response[0].cover_photo,
-        order:res.data.response[0].order,
-        chapters:res.data.response
-     }
+    try {
+        // let headers = { headers: { 'Authorization': `Bearer ${localStorage.getItem("token")}` } }
+        let res = await axios(apiUrl + "chapters/me?manga_id=" + obj.manga_id, headers())
+        return { 
+            title:res.data.response[0].title,
+            cover_photo:res.data.response[0].cover_photo,
+            order:res.data.response[0].order,
+            chapters:res.data.response
+        }
+    } catch (error) {
+        return {
+            title:null,
+            cover_photo:null,
+            order:null,
+            chapters:[]
+        }
+    }
 } )
 
 let saveTitle = createAction('saveTitle', (obj) => {
@@ -22,7 +30,6 @@ let saveTitle = createAction('saveTitle', (obj) => {
 let save_number = createAction('save_number', (obj) => {
     return { payload: { number: obj.number } };
 })
-
 
 const chapter_actions = { saveTitle, save_number, saveChapters };
 
