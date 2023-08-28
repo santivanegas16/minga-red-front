69bbedbebe2d4e52a21242ec6db66da93836690e
natigalo <natigalo1@gmail.com>
2023-08-27 22:29:00 -0500

    fix modal

--- src/components/Modal_form_update.jsx
+++ src/components/Modal_form_update.jsx
@@ -1,71 +1,110 @@
-import Close from '/img/close.png';
-import ListComments from "./ListComments"
+import { Link as Anchor, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-export default function Modal_comments({ show, setShow, reload, setReload }) {
-    return (
-        <>
-            <div className="fixed z-10 top-0 md:bg-black bg-[#EBEBEB] w-full h-full md:opacity-50"></div>
-            <div className='md:fixed absolute pt-[80px] md:pt-0 z-10 inset-y-[10%] md:inset-x-[25%]  w-full h-full'>
-                <div className="relative rounded-2xl w-full md:w-[50%] md:h-[80%] h-full bg-[#EBEBEB] md:overflow-auto  ">
-                    <div onClick={() => setShow(!show)} className="cursor-pointer absolute bg-orange-500 hover:bg-orange-700 p-1.5 rounded-full right-[15px] top-[15px]"><img src={Close} alt='close' /></div>
-                    <main className="flex w-full bg-[#EBEBEB]  min-h-screen pb-[30px]">
-                        <div className="flex flex-col justify-center lg:left-[50%] top-0 pt-[90px] items-center w-full lg:w-[50%]">
-                            <p className="font-poppins font-normal whitespace-nowrap left-[108px] text-[36px] lg:pt-0 w-[214px] text-center">
-                                New Manga
-                            </p>
+import manga_actions from "../store/actions/mangas";
+import { useState } from "react";
+const { destroy_manga } = manga_actions;
 
-                            <form className="flex flex-col lg:my-[10px] mt-[60px] items-center lg:mt-[10px]">
-                                <input
-                                    ref={title}
-                                    className="w-[280px] md:w-[350px] lg:w-[420px] bg-transparent outline-0 border-b-[1px] border-solid border-gray-700 px-3 pb-0 my-[12px] "
-                                    type="text"
-                                    name="title"
-                                    id="title"
-                                    placeholder="Insert title"
-                                />
-                                <select
-                                    defaultValue="0"
-                                    ref={category_id}
-                                    className="bg-transparent relative text-black w-[280px] md:w-[350px] lg:w-[420px] h-[24px] px-2 outline-0 border-b-[1px] border-solid border-gray-700 pb-0 mt-[20px] my-[12px]"
-                                    name="categories"
-                                    id="selectCat"
-                                >
-                                    <option disabled value="0" className="text-[#9D9D9D]">
-                                        Insert category
-                                    </option>
-                                    {categories?.map((item, index) => (
-                                        <option key={index} className="text-black" value={item._id}>
-                                            {item.name}
-                                        </option>
-                                    ))}
-                                </select>
-                                <input
-                                    ref={cover_photo}
-                                    className="w-[280px] md:w-[350px] lg:w-[420px] bg-transparent outline-0 border-b-[1px] border-solid border-gray-700 p-3 pb-0 my-[12px] "
-                                    type="url"
-                                    name="cover photo"
-                                    id="cover photo"
-                                    placeholder="Insert cover photo"
-                                />
-                                <input
-                                    ref={description}
-                                    className="w-[280px] md:w-[350px] lg:w-[420px] bg-transparent outline-0 border-b-[1px] border-solid border-gray-700 p-3 pb-0 my-[12px] "
-                                    type="text"
-                                    name="description"
-                                    id="description"
-                                    placeholder="Insert description"
-                                />
-                                <ButtonSend onClick={create} />
-                            </form>
-                        </div>
-                        <img
-                            className="hidden lg:block min-h-[640px] max-h-screen w-1/2 object-cover"
-                            src={Newmanga}
-                            alt="newmanga"
-                        />
-                    </main>
-                </div>
-            </div>
-        </>
-    )
+export default function Modal_form_update({ myManga }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [show, setShow] = useState(false);
+  console.log(myManga)
+  return (
+    <div className="bg-white w-[320px] h-[160px] lg:w-[420px] lg:h-[210px] m-3 flex justify-between items-center rounded-lg drop-shadow-md">
+      <span
+        className="w-2 h-[120px] lg:h-[160px] me-2"
+        style={{ backgroundColor: myManga.category_id.color }}
+      />
+      <div className="flex flex-col justify-between self-stretch py-4">
+        <div className="flex">
+          <Anchor to={`/${manga._id}/chapter-form`}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-[20px] h-[20px] rounded-full m-1 p-0 border-solid border-2 border-black text-black hover:drop-shadow-xl cursor-pointer"
+              style={{ display: manga.display }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+          </Anchor>
+          <Anchor to={`/edit-chapters/${manga._id}`}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-[20px] h-[20px] rounded-full m-1 p-0 border-solid border-2 border-black text-black hover:drop-shadow-xl cursor-pointer"
+              style={{ display: manga.display }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+              />
+            </svg>
+          </Anchor>
+        </div>
+        <div className="flex-grow">
+          <p className="text-[18px] lg:text-[24px] font-bold text-start font-montserrat">
+            {manga.title}
+          </p>
+          <p
+            className="text-[14px] lg:text-[20px] font-bold text-start font-montserrat"
+            style={{ color: manga.category_id.color }}
+          >
+            {manga.category_id.name}
+          </p>
+        </div>
+        <div className="flex">
+          <Anchor
+            to={"/edit-manga/" + manga._id}
+            className="rounded-full text-[12px] w-[70px] h-[25px] flex justify-center items-center mx-3 text-[#D1FBF0] bg-[#00BA88] hover:drop-shadow-xl cursor-pointer"
+            style={{ display: manga.display }}
+          >
+            EDIT
+          </Anchor>
+          <span
+            className="rounded-full text-[12px] w-[70px] h-[25px] flex justify-center items-center mx-3 text-[#FBDDDC] bg-[#EE8380] hover:drop-shadow-xl cursor-pointer"
+            style={{ display: manga.display }}
+            onClick={() =>
+              dispatch(
+                destroy_manga({
+                  id: manga._id,
+                  category: manga.category_id.name,
+                })
+              )
+            }
+          >
+            DELETE
+          </span>
+        </div>
+      </div>
+      {manga.to ? (
+        <Anchor to={manga.to}>
+          <img
+            className="w-[140px] h-[160px] lg:w-[190px] lg:h-[210px] object-cover rounded-[50%_0.5rem_0.5rem_50%] hover:drop-shadow-xl cursor-pointer"
+            src={manga.cover_photo}
+            alt={manga.title}
+          />
+        </Anchor>
+      ) : (
+        <Anchor to={`/manga/${manga._id}/1`}>
+          <img
+            className="w-[140px] h-[160px] lg:w-[190px] lg:h-[210px] object-cover rounded-[50%_0.5rem_0.5rem_50%] hover:drop-shadow-xl cursor-pointer"
+            src={manga.cover_photo}
+            alt={manga.title}
+          />
+        </Anchor>
+      )}
+    </div>
+  );
 }
